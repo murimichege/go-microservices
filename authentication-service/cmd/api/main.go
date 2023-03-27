be1ea7f7b8b289d01547e8ed4e4c12c808860f3a
murimichege <murimichege@gmail.com>
2023-03-27 18:04:06 +0300

    containerized the auth service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,31 +6,76 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "8081"
-type Config struct{
-	DB *sql.DB
+
+var count int64
+
+type Config struct {
+	DB     *sql.DB
 	Models data.Models
-
 }
-func main(){
+
+func main() {
 	log.Println(("Starting authentication service"))
-	 // toDo Connect to database
+	//  Connect to database
 
-	 // set up config
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Cant connect to Postgres")
+	}
 
-	 app:=Config{}
-	 svr := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+	// set up config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+	svr := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
-	 }
-	 
-	 err := svr.ListenAndServe()
-	 if err != nil {
+	}
+
+	err := svr.ListenAndServe()
+	if err != nil {
 		log.Panic(err)
-	 }
+	}
 
 }
 
-func openDB(dsn string) (*sql.DB)
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not ready")
+			count++
+		} else {
+			log.Println("COnnected to postgres")
+			return connection
+		}
+		if count > 10 {
+			log.Println(err)
+			return nil
+		}
+		log.Println("Backing off for two seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+
+}
--- broker-service/broker-service.dockerfile
+++ broker-service/broker-service.dockerfile
@@ -1,22 +1,7 @@
-#base go image
-FROM golang:1.19-alpine as builder
-
-RUN mkdir /app
-
-COPY . /app
-
-WORKDIR /app
-
-RUN CGO_ENABLED=D go build -o brokerApp ./cmd/api
-
-RUN chmod +x /app/brokerApp
-
-#build tiny docker image
-
 FROM alpine:latest
 
 RUN mkdir /app
 
-COPY --from=builder /app/brokerApp /app
+COPY brokerApp /app
 
-CMD ["app/brokerApp"]
+CMD [" app/brokerApp"]
